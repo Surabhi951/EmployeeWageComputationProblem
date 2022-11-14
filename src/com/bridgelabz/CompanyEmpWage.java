fd00b0e9bcc8828f5788142fd72137c0e303d318
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-14 23:32:15 +0530

    Modified: Managed employee wage of multiple companies

---
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+
+    final String company;
+    final int wagePerHour;
+    final int workingDayPerMonth;
+    final int workingHourPerMonth;
+    int totalWage;
+
+    public CompanyEmpWage(String company, int wagePerHour, int workingDayPerMonth, int workingHourPerMonth) {
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.workingDayPerMonth = workingDayPerMonth;
+        this.workingHourPerMonth = workingHourPerMonth;
+    }
+
+    @Override
+    public String toString() {
+        return "CompanyEmpWage{" +
+                "company='" + company + '\'' +
+                ", wagePerHour=" + wagePerHour +
+                ", workingDayPerMonth=" + workingDayPerMonth +
+                ", workingHourPerMonth=" + workingHourPerMonth +
+                ", totalWage=" + totalWage +
+                '}';
+    }
+}
--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,17 +4,11 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
-        EmpWageBuilder amazon = new EmpWageBuilder("Amazon", 20, 2, 10);
-        EmpWageBuilder meesho = new EmpWageBuilder("Meesho", 10, 4, 20 );
-        EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 3, 10);
-        EmpWageBuilder flipkart = new EmpWageBuilder("Flipkart", 15, 5, 30);
-        amazon.computeEmpWage();
-        System.out.println(amazon);
-        meesho.computeEmpWage();
-        System.out.println(meesho);
-        reliance.computeEmpWage();
-        System.out.println(reliance);
-        flipkart.computeEmpWage();
-        System.out.println(flipkart);
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage("Amazon", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Meesho", 10, 4, 20);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 3, 10);
+        empWageBuilder.addCompanyEmpWage("Flipkart", 15, 5, 30);
+        empWageBuilder.computeEmpWage();
     }
 }
